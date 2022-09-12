1e3fcb4c4f52084dfa1fc4a1e23075f9b7d4cb6e
Haiseong Jeong <wjdgotjd9908@gmail.com>
2022-09-12 15:07:21 +0900

    week2

---
+++ week2/2.c
@@ -0,0 +1,103 @@
+
+# include<stdio.h>
+# include<stdlib.h>
+
+typedef struct _list
+{
+	int *array;
+	int maxSize;
+	int size;
+} list;
+
+list *initList(int n)
+{
+	list *lst;
+
+	lst = (list *)malloc(sizeof(list));
+	lst->array = (int *)malloc(sizeof(int) * n);
+	lst->maxSize = n;
+	lst->size = 0; // 초기 사이즈 0으로 초기화
+
+	return lst;
+}
+
+int listSize(list *lst)
+{
+	return lst->size;
+}
+
+int isEmpty(list *lst)
+{
+	if (listSize(lst) == 0)
+		return 1;
+	else
+		return 0;
+}
+
+int isFull(list *lst)
+{
+	if (listSize(lst) == lst->maxSize)
+		return 1;
+	else
+		return 0;
+}
+
+void insertItem(list *lst, int item)
+{
+	int i;
+	int pos;
+
+	if (isFull(lst))
+		return ;
+
+	i = 0;
+	while (i < lst->size && lst->array[i] < item)
+		i++;
+	pos = i;
+	
+	for (i = lst->size; i >= pos + 1; i--)
+		lst->array[i] = lst->array[i - 1];
+
+	lst->array[pos] = item;
+	lst->size++;
+}
+
+int removeMin(list *lst)
+{
+	int i;
+	int min;
+
+	if (isEmpty(lst))
+		return -1;
+	min = lst->array[0];
+	for (i = 0; i < lst->size - 1; i++)
+		lst->array[i] = lst->array[i + 1];
+	lst->size--;
+	return min;
+}
+
+void freeList(list *lst)
+{
+	free(lst->array);
+	free(lst);
+}
+
+int main()
+{
+	list *lst;
+	int n;
+	int i, temp;
+
+	scanf("%d", &n);
+	lst = initList(n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &temp);
+		insertItem(lst, temp);
+	}
+	for (i = 0; i < n; i++)
+		printf(" %d", removeMin(lst));
+	
+	return 1;
+	freeList(lst);
+}
